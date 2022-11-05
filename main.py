7d8008edc17571c91892d3dfb3181d47b1d1c8cc
ghufronmalik64 <ghufronmalik64@gmail.com>
2022-11-06 01:59:42 +0700

    Menentukan Bilangan Terbesar Dari 2 Bilangan

---
+++ main.py
@@ -0,0 +1,14 @@
+print("Nama         :   Ghhufron Malik")
+print("Kelas        :   TI.22.B2")
+print("Mata Kuliah  :   Bahasa Pemrograman")
+print("===================================")
+
+A = int(input("Masukkan Bilangan A:"))
+B = int(input("Masukkan Bilangan B:"))
+
+if A>B:
+    maks = A
+else:
+    maks = B
+
+print("BILANGAN TERBESAR ADALAH: ", maks)
